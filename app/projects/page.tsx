d97d6e6c0fe1e95a7cf447d275dd6f26c6a545d3
Thiago Nunes <49001883+thiagonunes85@users.noreply.github.com>
2024-01-11 00:44:13 -0300

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import { Article } from "./article";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
 
@@ -21,22 +20,6 @@ export default async function ProjectsPage() {
   }, {} as Record<string, number>);
 
   const featured = allProjects.find((project) => project.slug === "unkey")!;
-  //const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  //const top3 = allProjects.find((project) => project.slug === "highstorm")!;
-  const sorted = allProjects
-
-    .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.slug !== featured.slug &&
-        //project.slug !== top2.slug &&
-        //project.slug !== top3.slug,
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
-    );
 
   return (
     <div className="relative pb-16">
@@ -47,28 +30,43 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-            Coming Soon.
+            Some of the projects are from work and some are on my own time.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
 
         <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          {/* Card para "Coming Soon" */}
           <Card>
             <article className="relative w-full h-full p-4 md:p-8">
-              <div className="text-xs text-zinc-100">
-                <span>SOON</span>
+              <div className="flex items-center justify-between gap-2">
+                <div className="text-xs text-zinc-100">
+                  <span>SOON</span>
+                </div>
+                <span className="flex items-center gap-1 text-xs text-zinc-500">
+                  <Eye className="w-4 h-4" />{" "}
+                  {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+                    views[featured.slug] ?? 0,
+                  )}
+                </span>
               </div>
-              <h2 className="mt-4 text-3xl font-bold text-zinc-100">
-                Coming Soon
+
+              <h2
+                id="featured-post"
+                className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+              >
+                {featured.title}
               </h2>
-              <p className="mt-4 leading-8 duration-150 text-zinc-400">
-                Exciting project coming soon! Stay tuned.
+              <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                {featured.description}
               </p>
+              <div className="absolute bottom-4 md:bottom-8">
+                <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </p>
+              </div>
             </article>
           </Card>
-
-        {/* Restante do código... */}
+        </div>
       </div>
     </div>
   );

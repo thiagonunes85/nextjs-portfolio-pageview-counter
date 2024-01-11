6faf59990f87fc7f9521d4c9ed523f609afbb89b
Thiago Nunes <49001883+thiagonunes85@users.noreply.github.com>
2024-01-10 22:56:25 -0300

    1
    1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { Github, Mail, Twitter, Instagram } from "lucide-react";
+import { Github, Mail, Instagram } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:dev@chronark.com",

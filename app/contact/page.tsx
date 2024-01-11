ff9eec4be82ca9c80a62d5a7a5b65c590ce61d88
Thiago Nunes <49001883+thiagonunes85@users.noreply.github.com>
2024-01-10 23:01:03 -0300

    1
    1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Instagram, Linkedin } from "lucide-react";
+import { Github, Mail, Instagram } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -23,12 +23,6 @@ const socials = [
 		label: "Instagram",
 		handle: "your_instagram_handle",
 	},
-	{
-		icon: <Linkedin size={20} />,
-		href: "https://www.linkedin.com/in/your_linkedin_profile/",
-		label: "LinkedIn",
-		handle: "your_linkedin_profile",
-	},
 ];
 
 export default function Example() {
@@ -36,13 +30,13 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mx-auto mt-32 sm:mt-0 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s, index) => (
 						<Card key={index}>
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
 								<span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
@@ -52,4 +46,18 @@ export default function Example() {
 									{s.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font
+									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+										{s.handle}
+									</span>
+									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+										{s.label}
+									</span>
+								</div>
+							</Link>
+						</Card>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}

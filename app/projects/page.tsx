65a5329ef37458179c7f08cd44eac5fe50809baf
Thiago Nunes <49001883+thiagonunes85@users.noreply.github.com>
2024-01-11 00:52:13 -0300

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -27,10 +27,10 @@ export default async function ProjectsPage() {
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            Projects
+            Comming Soon!
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some of the projects are from work and some are on my own time.
+            Stay updated soon.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
